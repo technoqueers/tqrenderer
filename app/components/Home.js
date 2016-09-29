9496e61a2a3f3ddc67d497414bdd5ed350665bac
bosgood <bosgood@gmail.com>
2016-09-28 23:42:01 -0400

    Simple output window in home screen

--- app/components/Home.js
+++ app/components/Home.js
@@ -4,10 +4,13 @@ import styles from './Home.css';
 export default class Home extends Component {
   render() {
     return (
-      <div>
-        <div className={styles.container}>
-          <h2>Home</h2>
-        </div>
+      <div className={styles.container}>
+        <header className={styles.header}>
+          TechnoQueers Email Generator
+        </header>
+        <textarea
+          className={styles.text}
+        />
       </div>
     );
   }
